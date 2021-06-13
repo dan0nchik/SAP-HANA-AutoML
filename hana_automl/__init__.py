49826d17518382bffd8ff7b823afffe41d33d286
Daniel Khromov <danielkhromov@gmail.com>
2021-06-13 19:18:39 +0300

    pypi update

--- hana_automl/__init__.py
+++ hana_automl/__init__.py
@@ -1,7 +1,7 @@
 """Welcome to hana_automl - Automated Machine Learning library based on SAP HANA.
     ******Installation*********
     1. pip3 install Cython
-    2. pip3 install --extra-index-url https://test.pypi.org/simple/ hana_automl
+    2. pip3 install hana_automl
 """
 
-__version__ = "1.6"
+__version__ = "1.7"
